52f39b94fdaf88934a088f77e7a75f8d09510f3a
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-09 19:11:56 +0530

    a

---
+++ eightfunction.py
@@ -0,0 +1,23 @@
+def addition(a,b):
+    print(a+b)
+
+def aakash(name):
+    print("SayHi "+name)
+
+def addtion(a,b):
+    return a+b
+
+def addition(a,b):
+    if a == 1:
+        return a+b
+    else:
+        return a-b
+
+#you have make function
+#that will take input from the user
+#and tell the user given value is odd or even
+
+#you have one list of names
+#you have to verify the user 
+#if user is part of list you say welcom
+#else you have to say sorry not allowed
---
+++ sevenForloop.py
@@ -0,0 +1,38 @@
+names = ['aakash', 'bhumika', 'nancy', 'rahul', 'shaheela','manisha','ayasha']
+for mynames in names:
+    if mynames == "shaheela":
+        break
+    print(mynames)
+
+
+#########
+for a in range(5):
+    print(a)
+
+for a in range(5,9):
+    print(a)
+
+for a in range(1,20,3):
+    print(a)
+
+
+###################
+i=5
+while i<10:
+    print(i)
+    if i == 8:
+        break
+    i += 1
+5
+6
+7
+8
+
+for x in range(5,10):
+    if x == 8:
+        break
+    print(x)
+
+5
+6
+7
